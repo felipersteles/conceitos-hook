d6e8e1de289f99f5b4e61a0c5d7ff02246e8e868
Felipe Teles <felipersteles@gmail.com>
2022-10-10 13:50:50 -0300

    useref commit

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,21 +1,73 @@
-import React, { useState } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    const [numero, alterarNumero] = useState(1);
+function calcFatorial(n) {
+  if (n < 0) return -1;
+  if (n === 0) return 1;
 
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-
-            <div className="center">
-                <input type="number" className="input" value={numero} onChange={evento => alterarNumero(evento.target.value) } />
-            </div>
-        </div>
-    )
+  return n * calcFatorial(n - 1);
 }
 
-export default UseEffect
+function ehPar(numero) {
+  if (numero % 2 === 0) return true;
+  else return false;
+}
+
+// Efeito colateral: modificar algo dentro da interface alterando outro tipo de informação que pertence ao componente
+const UseEffect = (props) => {
+  const [numero, alterarNumero] = useState(1);
+  const [fatorial, alterarFatorial] = useState(1);
+  const [par, alterarPar] = useState(false);
+
+  // alterarFatorial(calcFatorial(numero))
+  // a linha de codigo acima gera um loop infinito pois ele vai fazer o calculo o tempo todo
+  // o certo é calcular somente quando o valor for alterado
+
+  //rece dois paramentros sendo um deles o efeito e o segundo a dependencia
+  useEffect(
+    function () {
+      alterarFatorial(calcFatorial(numero));
+    },
+    [numero]
+  ); //quando esse valor for modificado a função é chamada
+
+  useEffect(() => {
+    alterarPar(ehPar(numero));
+  }, [numero]);
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercio #01" />
+
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={numero}
+          onChange={(evento) => alterarNumero(evento.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercio #02" />
+      <div className="center">
+        <div>
+          <span className="text">É par? </span>
+          <span className="text red">{par === true ? "sim" : "não"}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useRef, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+  const [valor1, alterarValor1] = useState("");
+  const contador = useRef(0); //retorna um objeto e sempre vai retonar a mesma ref
 
-export default UseRef
+  // passagem por referencia é quando o valor de um objeto para uma variavel
+  // e atribui o valor dessa variavel para outra variavel
+
+  useEffect(
+    function () {
+      contador.current = contador.current + 1;
+    },
+    [valor1]
+  );
+
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercio #01" />
+      <div className="center">
+        <div>
+          <span className="text">Valor: </span>
+          <span className="text">{valor1} [</span>
+          <span className="text red">{contador.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          type="text"
+          className="input"
+          value={valor1}
+          onChange={(evento) => alterarValor1(evento.target.value)}
+        />
+          </div>
+          
+          <SectionTitle title="Exercio #02" />
+    </div>
+  );
+};
+
+export default UseRef;
